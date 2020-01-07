d69e6a1d83666395e795a1b201c7d8efffa594e1
ArtemTropanets <36934520+ArtemTropanets@users.noreply.github.com>
2020-01-07 21:52:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.querySelector('.menu-hamburger').addEventListener('click', function() {
 	this.classList.toggle('menu-hamburger-active');
 	document.querySelector('.nav__list').classList.toggle('nav__list-active');
 });
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	loop: true,
+  	nav: true,
+  	items: 1
+  });
+});
